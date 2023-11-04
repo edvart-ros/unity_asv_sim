6ddcf830695f62d5b759cb533597c0e0d2bc3e58
edvart-ros <240818@usn.no>
2023-11-04 01:29:13 +0100

    possibly fixed Unity -> NED data collection

--- Assets/Scripts/sysid/CollectData.cs
+++ Assets/Scripts/sysid/CollectData.cs
@@ -13,39 +13,47 @@ public class CollectData : MonoBehaviour
     private float rightForce;
     private float yawTorque;
 
+    public float FwdMaxF = 500f;
+    public float RightMaxF = 500f;
+    public float YawMaxT= 500f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         data = new List<string>();
         controlTimesteps = Random.Range(40, 300);
         controlTimestepsAcc = 0;
-        fwdForce = Random.Range(-500f, 500f);
-        rightForce = Random.Range(-500f, 500f);
-        yawTorque = Random.Range(-500f, 500f);
+        fwdForce = Random.Range(-FwdMaxF, FwdMaxF);
+        rightForce = Random.Range(-RightMaxF, RightMaxF);
+        yawTorque = Random.Range(-YawMaxT, YawMaxT);
     }
 
     void FixedUpdate()
     {
         if (controlTimestepsAcc > controlTimesteps){ // if the control has been exerted for the alloted timesteps Kt
-            fwdForce = Random.Range(-500f, 500f);
-            rightForce = Random.Range(-500f, 500f);
-            yawTorque = Random.Range(-500f, 500f);
+            fwdForce = Random.Range(-FwdMaxF, FwdMaxF);
+            rightForce = Random.Range(-RightMaxF, RightMaxF);
+            yawTorque = Random.Range(-YawMaxT, YawMaxT);
             controlTimesteps = Random.Range(40, 300);
             controlTimestepsAcc = 0;
         }
         rb.AddRelativeForce(fwdForce*Vector3.forward + rightForce*Vector3.right);
-        rb.AddRelativeTorque(yawTorque*-Vector3.up);
+        rb.AddRelativeTorque(yawTorque*Vector3.up);
 
         Vector3 position = rb.position;
-        Vector3 localVelocity = rb.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
         Vector3 localAngularVelocity = rb.angularVelocity;
         Vector3 rpy = rb.rotation.eulerAngles;
+
+        float yawDeg = rpy.y;
+        float yawRad = Mathf.Deg2Rad * yawDeg; // Convert to radians
+        yawRad = NormalizeYaw(yawRad);
 
         // Store data
 
         // using NED coordinate system, converting from unity. capturing 6DOF data. eta = [N, E, psi, u, v, r,]^T
         // [Timestamp, N (z), E (x), psi (y-axis rotation, left hand), u (Vz), v (Vx), r (Wy), F_N (F.z), F_E (F.x), T_psi (T.y)]. Enclosed in parentheses: Unity equivalent
-        data.Add($"{Time.time}, {position.z}, {position.x}, {rpy.y}, {localVelocity.z}, {localVelocity.x}, {-localAngularVelocity.y}, {fwdForce}, {rightForce}, {yawTorque}");
+        data.Add($"{Time.time}, {position.z}, {position.x}, {yawRad}, {localVelocity.z}, {localVelocity.x}, {localAngularVelocity.y}, {fwdForce}, {rightForce}, {yawTorque}");
         controlTimestepsAcc++;
     }
 
@@ -79,5 +87,18 @@ public class CollectData : MonoBehaviour
         {
             Debug.LogError("Error saving data: " + ex.Message);
         }
+    }
+    
+    float NormalizeYaw(float yaw)
+    {
+        while (yaw > Mathf.PI)
+        {
+            yaw -= 2 * Mathf.PI;
+        }
+        while (yaw <= -Mathf.PI)
+        {
+            yaw += 2 * Mathf.PI;
+        }
+        return yaw;
     }
 }
