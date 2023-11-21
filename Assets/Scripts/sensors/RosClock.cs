0b37d79c3e82d2054f6fe6e222786e657e41ec3c
edvart-ros <240818@usn.no>
2023-11-21 14:16:30 +0100

    changes: publishing simulation time on /clock Fixed the PointField "count" (now just set to 1)

---
+++ Assets/Scripts/sensors/RosClock.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Rosgraph;
+using Unity.Robotics.Core;
+     
+public class ClockPublisher : MonoBehaviour
+{   
+    [SerializeField] private string _topicName = "clock";
+     
+    private ROSConnection _ros;
+    private ClockMsg _message;
+    public uint sec, nanosec;    
+    double m_PublishRateHz = 200f;
+    double m_LastPublishTimeSeconds;
+    double PublishPeriodSeconds => 1.0f / m_PublishRateHz;
+    bool ShouldPublishMessage => Clock.FrameStartTimeInSeconds - PublishPeriodSeconds > m_LastPublishTimeSeconds;
+     
+    void Start()
+    {
+        // setup ROS
+        _ros = ROSConnection.GetOrCreateInstance();
+        _ros.RegisterPublisher<ClockMsg>(_topicName);
+        _message = new ClockMsg();
+        _message.clock.sec = 0;
+        _message.clock.nanosec = 0;
+    }
+     
+     
+       
+    void PublishMessage()
+    {
+        var publishTime = Clock.Now;
+        sec = (uint)publishTime;
+        nanosec = (uint)((publishTime - Math.Floor(publishTime)) * Clock.k_NanoSecondsInSeconds);
+        _message.clock.sec = (int)sec;
+        _message.clock.nanosec = nanosec;
+        m_LastPublishTimeSeconds = publishTime;
+        _ros.Publish(_topicName, _message);
+    }
+     
+    private void FixedUpdate()
+    {
+        if (ShouldPublishMessage)
+        {
+            PublishMessage();
+        }
+    }
+     
+}
