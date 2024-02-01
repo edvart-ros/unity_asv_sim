eb2c8435e5e4006cb3193f3e3d1522099d78d4cb
edvart-ros <240818@usn.no>
2024-02-01 18:24:44 +0200

    Revert "Changes:"
    This reverts commit 5bbd0f36cef73e20bdf3e9ce211d5719f28be9d0.

--- Assets/Scripts/water_interaction/processing/Submersion.cs
+++ Assets/Scripts/water_interaction/processing/Submersion.cs
@@ -30,7 +30,7 @@ public class Submersion : MonoBehaviour
     void Update()
     {
         patch.Update(transform); // updates the patch to follow the boat and queried water height
-        submerged.Update2(patch, transform);
+        submerged.Update(patch, transform);
         if (drawPatch) DebugPatch();
         if (drawSubmerged) DebugSubmerged();
     }
@@ -48,16 +48,14 @@ public class Submersion : MonoBehaviour
 
     private void DebugSubmerged()
     {
-        float[] centerHeights = submerged.FaceCenterHeightsAboveWater;
         int[] tris = submerged.mesh.triangles;
         Vector3[] verts = submerged.mesh.vertices;
-        Debug.Log(tris.Length/3);
-        for (int i = 0; i < tris.Length - 2; i += 3) {
+        for (var i = 0; i < tris.Length; i += 3)
+        {
             Vector3[] tri = new Vector3[] { 
                 transform.TransformPoint(verts[tris[i]]), 
                 transform.TransformPoint(verts[tris[i+1]]), 
                 transform.TransformPoint(verts[tris[i+2]])};
-
             Utils.DebugDrawTriangle(tri, Color.green);
         }
     }
