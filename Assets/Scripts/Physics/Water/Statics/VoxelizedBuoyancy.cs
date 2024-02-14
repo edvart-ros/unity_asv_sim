e00318864933b6b685f4370d2ea96d6824146dc6
u-random <isaklars@outlook.com>
2024-02-14 09:30:03 +0100

    WiP
    Trying to add hydrodynamics to Voxel system. Not trivial!

--- Assets/Scripts/Physics/Water/Statics/VoxelizedBuoyancy.cs
+++ Assets/Scripts/Physics/Water/Statics/VoxelizedBuoyancy.cs
@@ -1,8 +1,12 @@
+using System;
+using UnityEngine.Rendering.HighDefinition;
 using System.Collections.Generic;
 using UnityEditor.Playables;
 using System.Collections;
 using UnityEngine;
 using System.IO;
+using Physics.Water.Dynamics;
+using WaterInteraction;
 using TMPro;
 
 
@@ -10,22 +14,21 @@ using TMPro;
 // It obviously has something to do with the amount of points, and difference between that is not accounted for
 // there seems to be about a .5 or .4 difference in mass to maintain the same waterline,
 // from 6 voxels to 3 voxels (15000 to 8500)
-// TODO: Investigate difference in mass contribution from Kerner model 5 000 000 to voxel model 15 000
 // TODO: Consider factoring in Depth of the point to buoyancy force
-
-// TODO: ADD support for water patch
+// TODO: Find Voxels at waterling, i.e. height is less than radius +- above the water
 
 
 public class VoxelizedBuoyancy : MonoBehaviour
 {
+    [Tooltip("Leave empty to toggle Plane buoyancy off.")]
     public Transform planeTransform;
     
     public TextMeshProUGUI buoyancyText;
     
     private List<Vector3> pointsInsideMesh = new List<Vector3>();
     private string path = "Assets/Data/localPointsData.json";
-    private List<Vector3> globalPositions = new List<Vector3>();
-    private List<Vector3> relativePositions = new List<Vector3>();
+    private List<Vector3> globalVoxelPositions = new List<Vector3>();
+    //private List<Vector3> relativePositions = new List<Vector3>();
     private Vector3 parentPosition;
 
     private Transform parentTransform;
@@ -33,7 +36,14 @@ public class VoxelizedBuoyancy : MonoBehaviour
     private float actualForce;
     private float voxelVolume;
 
-
+    // Water querying variables
+    [Tooltip("Set to zero to toggle water patch off.")]
+    public float patchSize = 10;
+    public int patchResolution = 4;
+    public WaterSurface targetSurface = null;
+    private Patch patch;
+    public bool drawPatch;
+    
     private Rigidbody shipRigidbody;
     
     
@@ -46,16 +56,41 @@ public class VoxelizedBuoyancy : MonoBehaviour
         shipRigidbody = GetComponent<Rigidbody>();
     }
 
-    
+
+    private void Start() // Water patch setup
+    {
+        if (patchSize != 0)
+        {
+            Vector3 gridOrigin = new Vector3(-patchSize/2, 0, patchSize/2);
+            patch = new Patch(targetSurface, patchSize, patchResolution, gridOrigin);
+            patch.Update(transform);
+        }
+    }
+
     private void FixedUpdate()
     {
-        UpdateGlobalPosition();
-        ApplyForce(CalculateCenterOfPoints(GetPointsUnderPlane()));
+        UpdateGlobalVoxelPosition();
+        if (planeTransform)
+        {
+            ApplyForce(CalculateCenterOfPoints(GetPointsUnderPlane()));
+        }
+        if (patchSize != 0)
+        {
+            patch.Update(transform);
+            ApplyForce(CalculateCenterOfPoints(GetPointsUnderWaterPatch()));
+            List<Vector3> debugPoints = GetPointsUnderWaterPatch();
+            foreach (var point in debugPoints)
+            {
+                Debug.DrawRay(point, Vector3.up, Color.red);
+            }
+        }
         
+        if (drawPatch) Utils.DrawPatch(patch);
         buoyancyText.text = "Buoyancy Force: " + actualForce.ToString("F2");
     }
 
 
+    /// Returns the list of points under the Plane.
     private List<Vector3> GetPointsUnderPlane()
     {
         // Create a Plane object using the position and up vector of the planeTransform
@@ -63,12 +98,26 @@ public class VoxelizedBuoyancy : MonoBehaviour
         
         List<Vector3> pointsUnderPlane = new List<Vector3>();
 
-        foreach (var point in globalPositions)
+        foreach (var point in globalVoxelPositions)
         {
             if (!plane.GetSide(point)) pointsUnderPlane.Add(point);
         }
 
         return pointsUnderPlane;
+    }
+    
+    
+    /// Returns the list of points under the water patch.
+    public List<Vector3> GetPointsUnderWaterPatch()
+    {
+        List<Vector3> pointsUnderPatch = new List<Vector3>();
+
+        foreach (var point in globalVoxelPositions)
+        {
+            if (patch.GetPatchRelativeHeight(point) < 0.00001f) pointsUnderPatch.Add(point);
+        }
+
+        return pointsUnderPatch;
     }
     
     
@@ -83,28 +132,27 @@ public class VoxelizedBuoyancy : MonoBehaviour
     }
 
 
-    private void ApplyForce((Vector3 centerOfMass, int numberOfPoints) data)
+    private void ApplyForce((Vector3 centerOfBuoyancy, int numberOfPoints) data)
     {
-        float force = CalculateForce(data.numberOfPoints, voxelVolume, 9.81f);
-        shipRigidbody.AddForceAtPosition(force*Vector3.up, data.centerOfMass);
-        Debug.DrawRay(data.centerOfMass,force*Vector3.up, Color.red);
+        float force = CalculateForce(data.numberOfPoints, voxelVolume);
+        shipRigidbody.AddForceAtPosition(force*Vector3.up, data.centerOfBuoyancy);
+        Debug.DrawRay(data.centerOfBuoyancy,force*Vector3.up, Color.red);
     }
 
 
-    private float CalculateForce(int numberOfPoints, float volume, float gravity)
+    private float CalculateForce(int numberOfPoints, float volume)
     {
-        return actualForce = numberOfPoints * volume * gravity;
-        //return actualForce;
+        return actualForce = Constants.waterDensity * numberOfPoints * volume * Constants.gravity;
     }
     
     
-    private void UpdateGlobalPosition()
+    private void UpdateGlobalVoxelPosition()
     {
         if (!transform.hasChanged) return;
-        globalPositions.Clear();
+        globalVoxelPositions.Clear();
         foreach (Vector3 point in pointsInsideMesh)
         {
-            globalPositions.Add(transform.TransformPoint(point));
+            globalVoxelPositions.Add(transform.TransformPoint(point));
         }
         // Reset the hasChanged flag
         transform.hasChanged = false;
@@ -118,11 +166,23 @@ public class VoxelizedBuoyancy : MonoBehaviour
     }
     
     
+    void ApplyHydrodynamicDampening() 
+    {
+        //foreach (var point in submergedPoints) 
+        {
+            //Vector3 velocity = GetPointVelocity(point); // Implement this method based on your system
+            //float area = CalculateCrossSectionalArea(point); // Implement based on voxel size or shape
+            //Vector3 dragForce = CalculateDragForce(shipRigidbody.velocity, area, dragCoefficient);
+            //ApplyDragForce((point, dragForce)); // You'll need to implement how forces are applied in your system
+        }
+    }
+    
+    
     private void OnDrawGizmos()
     {
-        if (globalPositions.Count == 0) return;
+        if (globalVoxelPositions.Count == 0) return;
         Gizmos.color = Color.magenta;
-        foreach (Vector3 point in globalPositions)
+        foreach (Vector3 point in globalVoxelPositions)
         {
             Gizmos.DrawSphere(point, 1); 
         }
